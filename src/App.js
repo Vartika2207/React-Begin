9edd100dd3aab4df0b9e9762ea29ebfef7359d02
Vartika2207 <vartika.singhania99@gmail.com>
2023-10-02 23:46:57 +0530

    added jsx

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 // import './App.css';
 
+
 function App() {
+  function formatUser(user) {
+    return (
+        user.firstname + " " + user.lastname
+    )
+  }
+
+  function getGreeting(user) {
+    if(user) {
+        return <h1>Hello {user.firstname}</h1>
+    }
+    else {
+        return <h1>Hello Customer</h1>
+    }
+  }
+
+  const element = <h1>Hello there</h1>
+  const name = {
+    firstname: 'Vartika',
+    lastname: 'Shakya'
+  }
   return (
     <div>
-      <h1>Hey there</h1>
+      <h2>{element}</h2>
+      <h3>{formatUser(name)}</h3>
+      {getGreeting(name)}
     </div>
   );
 }
+
+App();
 
 export default App;
