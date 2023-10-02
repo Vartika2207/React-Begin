e61b0cf124f5278f2234b6e7e27f55412943768e
Vartika2207 <vartika.singhania99@gmail.com>
2023-10-03 00:00:16 +0530

    redering elements

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+// import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+function tick() {
+  const element = (
+    <div>
+      <h2>Hello, the time is {new Date().toLocaleTimeString()}</h2>
+      
+    </div>
+  )
+
+  root.render(
+    <React.StrictMode>
+      <div>{element}</div>
+    </React.StrictMode>
+  );
+  
+}
+
+setInterval(tick, 1000);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
